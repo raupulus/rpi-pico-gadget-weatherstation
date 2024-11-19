4a4870ec7629fb12bcda903c77504fd74d7f55d1
Raúl Caro Pastorino <raul@fryntiz.dev>
2024-11-19 19:48:06 +0100

    Added api upload control and display params

--- src/.env.example.py
+++ src/.env.example.py
@@ -9,12 +9,17 @@ ALTERNATIVES_AP = [
 ]
 
 # Datos para la API
+API_UPLOAD = True
 API_URL = "http://localhost:8000/api"
 API_PATH = "path/to/endpoint"
 API_TOKEN = "apitoken"
 
 # Nombre del equipo para identificarlo en la api, id y nombre.
 DEVICE_ID = 0
+
+# Configuración de la pantalla.
+DISPLAY_ORIENTATION = 3
+DISPLAY_TIMEOUT = 10 # Minutos para apagar la pantalla automáticamente
 
 # Indica si está en modo debug la aplicación
 DEBUG = False
